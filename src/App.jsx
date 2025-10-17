f439297aab6c1d4522b66a987f2d40f96814fc0a
AlessandroCarella <alessandro.carella.universita@gmail.com>
2025-10-18 00:47:59 +0200

    switch to vite react deployment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+            <p>This is a minimal React app deployed to GitHub Pages.</p>
+        </div>
+    );
+}
+
+export default App;
