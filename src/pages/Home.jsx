41ed5c90192edfc3f5d9018305c39e507fb34f99
AlessandroCarella <alessandro.carella.universita@gmail.com>
2025-10-21 20:28:35 +0200

    changed the size of the text in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-
 import TextPressure from "../components/TextPressure";
-import SplitText from "../components/SplitText";
 import ShinyText from "../components/ShinyText";
-import DecryptedText from "../components/DecryptedText";
 import GitHubCalendar from "react-github-calendar";
 
 import "./styles/Home.css";
@@ -20,7 +16,6 @@ const Home = () => {
                         className="sidebar-image"
                     />
                     <div className="sidebar-info">
-                        <h2 className="heading-lg">Alessandro Carella</h2>
                         <p className="paragraph">
                             MSc Data Science & Business Informatics
                         </p>
@@ -48,12 +43,12 @@ const Home = () => {
                 <div className="max-w-6xl mx-auto px-6 py-16">
                     <h1 className="heading-xl">About Me</h1>
                     <p className="paragraph">Hi, nice to see you here :)</p>
+                    {/* <p className="paragraph">
+                        On this page you will find a summary of my experiences
+                        both through projects and .
+                    </p> */}
                     <p className="paragraph">
-                        On this page you will find a summary of what I have
-                        studied and worked on over the years.
-                    </p>
-                    <p className="paragraph">
-                        I have worked on many projects during the years. I have
+                        I have worked on many projects over the years. I have
                         decided to showcase some of them in the Project
                         page[link to page]. The page will ask you to select
                         which kind of projects you are interested in seeing
@@ -61,25 +56,22 @@ const Home = () => {
                     </p>
                     <p className="paragraph">
                         The about page allows you to get to know me a bit
-                        better, I talk about my hobbies and some fun
-                        facts.
+                        better, I talk about my hobbies and some fun facts.
                     </p>
                     <div className="paragraph">
-                        In the resume page you can find a 1:1 copy of the info I
-                        shared with many companies in a standardized way so that
-                        they could <s>hire me</s>{" "}
-                        <ShinyText
-                            text=" suggest me the best fitting ads on my social media feed."
-                            speed={3}
-                        />
-                        {/* <DecryptedText
-                            text=" suggest me the best fitting ads on my social media feed."
-                            speed={25}
-                            animateOn={"view"}
-                            revealDirection={"start"}
-                            maxIterations={10}
-                            sequential={true}
-                        /> */}
+                        In the resume page, you can find a 1:1 copy of the info
+                        I shared with many companies in a standardized way so
+                        that they could <s>hire me</s>{" "}
+                        <ShinyText text="suggest" speed={3} />{" "}
+                        <ShinyText text="the" speed={3} />{" "}
+                        <ShinyText text="best" speed={3} />{" "}
+                        <ShinyText text="fitting" speed={3} />{" "}
+                        <ShinyText text="ads" speed={3} />{" "}
+                        <ShinyText text="on" speed={3} />{" "}
+                        <ShinyText text="my" speed={3} />{" "}
+                        <ShinyText text="social" speed={3} />{" "}
+                        <ShinyText text="media" speed={3} />{" "}
+                        <ShinyText text="feed." speed={3} />
                     </div>
                 </div>
                 <div className="max-w-6xl mx-auto px-6 py-16">
@@ -87,16 +79,16 @@ const Home = () => {
                     <p className="paragraph">
                         I am currently seeking a position as a data scientist in
                         a collaborative, fast-paced, and challenging
-                        environment. I enjoy solving problems using a data driven
-                        approach and apply my coding skills on top of my data
-                        intuition to find quick and adaptive solutions!
+                        environment. I enjoy solving problems using a data
+                        driven approach, and apply my coding skills on top of my
+                        data intuition to find quick and adaptive solutions!
                     </p>
                     <p className="paragraph">
                         I am a data scientist with a passion for visual
-                        storytelling. I believe that {" "}
+                        storytelling. I believe that{" "}
                         <b>
-                            words, data and its correct representation are the
-                            most powerful tools to change the world
+                            words, data, and its contextual representation are
+                            the most powerful tools to change the world
                         </b>
                         .
                     </p>
@@ -105,19 +97,15 @@ const Home = () => {
                     <h1 className="heading-xl">What I can do</h1>
                     <p className="paragraph">
                         My skills include{" "}
-                        <b>
-                            both data analysis and programming focused skills
-                        </b>
-                        .
-                        {/* I have developed a strong acumen for problem solving,
+                        <b>both data analysis and programming-focused skills</b>
+                        .{" "}
+                        {/* I have developed a strong acumen for problem-solving,
                         and I enjoy an occasional challenge. */}
-                    </p>
-                    <p className="paragraph">
-                        During my bachelor's degree I learned most of the
+                        During my bachelor's degree, I learned most of the
                         programming skills and techniques I put to use on a
-                        daily basis, also, for my data related projects. Those
+                        daily basis, for my data-related projects as well. Those
                         skills turned out to be very useful in the data
-                        visualization field, my master thesis [link to thesis
+                        visualization field, my master's thesis [link to thesis
                         repo] is in fact on the topic.
                     </p>
                     {/* <p className="paragraph">
@@ -132,7 +120,7 @@ const Home = () => {
                         Most of my time is spent staring at a computer screen.
                         During the day, I am usually working on some university,
                         personal or side project, as proven by my contribution
-                        chart on github
+                        chart on GitHub.
                     </p>
                     <div className="w-full overflow-x-auto">
                         <div className="github-calendar-home">
@@ -148,37 +136,38 @@ const Home = () => {
                         Previous work experience and new horizons
                     </h1>
                     <p className="paragraph">
-                        As you can see in my resume page [link to resume page] I
-                        worked as a full stack developer for 14 months, mostly
-                        on the frontend side of development. At work i used
-                        ReactJs and the Springboot framework, with the
-                        occasional jsp page here and there.
+                        As you can see in my resume page [link to resume page],
+                        I worked as a full-stack developer for 14 months, mostly
+                        on the frontend side of development. At work, i used
+                        ReactJS and the Springboot framework, with the
+                        occasional JSP page here and there.
                     </p>
                     <p className="paragraph">
-                        I am now looking forward to a data focused role but I
-                        would not mind a position where I can mix both of my
-                        developed expertise.
+                        I am now looking forward to a data-focused position
+                        where I can combine both of my developed areas of
+                        expertise.
                     </p>
                 </div>
                 <div className="max-w-6xl mx-auto px-6 py-16">
                     <h1 className="heading-xl">Additional Info </h1>
                     <p className="paragraph">
-                        I feel very lucky to be born in a setting that allowed
-                        me to learn so much among so many talented teachers and
-                        peers.
+                        I feel fortunate to be born in a setting that allowed me
+                        to learn so much among extraordinarily talented teachers
+                        and peers.
                     </p>
                     <p className="paragraph">
-                        My native language is Italian but, since I was a
-                        teenager, I have developed most of my interest online,
-                        therefore I do not really consider English a second
-                        language. Every class in my master degree was held in
+                        My native language is Italian, but since I was a
+                        teenager, I have developed most of my interests online;
+                        therefore, I do not really consider English as a second
+                        language. Every class in my master's degree was held in
                         English and I both bonded and worked, mostly with
-                        non-italian students and always in a setting where not
-                        everyone’s first language was italian.{" "}
+                        international students.
+                         {/* and <b>always</b> in a setting
+                        where not everyone's first language was Italian. */}
                     </p>
                     <p className="paragraph">
                         I'm excited to bring my skills to an exciting and
-                        challenging setting for my first data related job :)
+                        challenging setting for my first data-related job :)
                     </p>
                 </div>
             </main>
